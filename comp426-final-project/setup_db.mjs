b30cde086663e7b74a18fcdec490844e7c4c156a
Sebastian Zagler <97578874+szagler@users.noreply.github.com>
2024-04-30 22:58:33 -0400

    more stuff

--- comp426-final-project/setup_db.mjs
+++ comp426-final-project/setup_db.mjs
@@ -9,21 +9,7 @@ await db.run('CREATE TABLE IF NOT EXISTS movies (username VARCHAR(16), movie_id 
 await db.run('INSERT INTO users VALUES (?, ?)', 'scandium', 'password123');
 await db.run('INSERT INTO movies VALUES (?,?)', 'username', 'movie_id');
 
-let nodes_raw = (await db.all('select movie_id from movies where username = ?', username)).map(s => s.movie_id);
-let db_result = await db.run('delete from movies where movie_id = ?', movie_id);
-
-
-
-
-
-
-await db.run('CREATE TABLE IF NOT EXISTS nodes (id INTEGER PRIMARY KEY, headline TEXT(100) NOT NULL, note TEXT(100) NOT NULL)');
-await db.run('CREATE TABLE IF NOT EXISTS edges (parent_id INTEGER, child_id INTEGER)');
-//await db.run('CREATE TABLE IF NOT EXISTS edges (parent_id INTEGER, child_id INTEGER), PRIMARY KEY (parent_id, child_id), CONSTRAINT parent FOREIGN KEY (parent_id) REFERENCES nodes(id), CONSTRAINT child FOREIGN KEY (child_id) REFERENCES nodes(id)');
-let node_id = 0;
-let node_headline = 'root';
-let node_note = '';
-await db.run('DELETE FROM nodes');
-await db.run('INSERT INTO nodes VALUES (?, ?, ?)', node_id, node_headline, node_note);
+let my_movies = (await db.all('select movie_id from movies where username = ?', username)).map(s => s.movie_id);
+let db_result = await db.run('delete from movies where username = ? AND movie_id = ?', username, movie_id);
 
 db.close();

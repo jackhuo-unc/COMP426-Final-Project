e32170c48607211e2d9936b9e6ee0b7e33f85206
DerivativeDemons_309-at-715158845633 <jackhuo@unc.edu>
2024-04-30 22:32:18 -0400

    starting over

--- comp426-final-project/src/main.server.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { MainComponent } from './app/main/main.component';
-import { config } from './app/app.config.server';
-
-const bootstrap = () => bootstrapApplication(MainComponent, config);
-
-export default bootstrap;
